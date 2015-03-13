7ee5d839c9b5323ed4ead36497e8bc5c12591881
Mikhail Cruz Andrade <jailandrade@gmail.com>
2015-03-13 17:44:34 -0600

    cambiando el favicon

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="shorcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
