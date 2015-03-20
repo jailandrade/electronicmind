310f81b6e76c412b0fa27c806968134f89c66d6b
Mikhail Cruz Andrade <jailandrade@gmail.com>
2015-03-19 19:30:51 -0600

    agregando la radio

--- header.php
+++ header.php
@@ -21,6 +21,13 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 
+	<div id="radio" class="site-radio">
+		<object id="dewplayer-vol" width="100%" height="20" type="application/x-shockwave-flash" data="http://www.mexiserver.com/reproductores/dewplayer-vol.swf?autoplay=true&amp;mp3=http://204.12.193.98:8315/;stream.nsv">
+		<param name="wmode" value="transparent" />
+		<param name="movie" value="http://www.mexiserver.com/reproductores/dewplayer-vol.swf?autoplay=true&amp;mp3=http://204.12.193.98:8315/;stream.nsv" />
+		</object>
+	</div>
+
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<a href="<?php bloginfo('url'); ?>"><img class="site-logo" src="<?php echo bloginfo('template_directory');?>/assets/img/logo.png"></a>
