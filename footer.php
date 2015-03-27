a0b5005178019675b2de15c9082972f39e45342d
Mikhail Cruz Andrade <jailandrade@gmail.com>
2015-03-27 16:09:13 -0600

    arreglando footer

--- footer.php
+++ footer.php
@@ -31,22 +31,22 @@
 				<h3>Siguenos en: </h3>
 				<ul>
 					<li class="facebook-icon">
-						<a href="<?php esc_url(http://www.facebook.com/ElecMind); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/facebook.png"></a>
+						<a href="<?php esc_url('http://www.facebook.com/ElecMind'); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/facebook.png"></a>
 					</li>
 					<li class="twitter-icon">
-						<a href="<?php esc_url(http://www.twitter.com/#!/Elecmind); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/twitter.png"></a>
+						<a href="<?php esc_url('http://www.twitter.com/#!/Elecmind'); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/twitter.png"></a>
 					</li>
 					<li class="googleplus-icon">
-						<a href="<?php esc_url(https://plus.google.com/u/0/109919203968486042477/posts); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/googleplus.png"></a>
+						<a href="<?php esc_url('https://plus.google.com/u/0/109919203968486042477/posts'); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/googleplus.png"></a>
 					</li>
 					<li class="soundcloud-icon">
-						<a href="<?php esc_url(https://soundcloud.com/elecmind); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/soundcloud.png"></a>
+						<a href="<?php esc_url('https://soundcloud.com/elecmind'); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/soundcloud.png"></a>
 					</li>
 					<li class="youtube-icon">
-						<a href="<?php esc_url(https://www.youtube.com/user/ElectronicMindEM); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/youtube.png"></a>
+						<a href="<?php esc_url('https://www.youtube.com/user/ElectronicMindEM'); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/youtube.png"></a>
 					</li>
 					<li class="mixcloud-icon">
-						<a href="<?php esc_url(http://www.mixcloud.com/elecmind/); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/mixcloud.png"></a>
+						<a href="<?php esc_url('http://www.mixcloud.com/elecmind/'); ?>" target="_blank"><img src="<?php echo bloginfo('template_directory');?>/assets/img/mixcloud.png"></a>
 					</li>
 				</ul>
 			</div>
