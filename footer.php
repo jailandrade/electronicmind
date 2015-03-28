10c1b068a6deb7e4a8eef8ece4bcb4fb09ad1dce
Mikhail Cruz Andrade <jailandrade@gmail.com>
2015-03-27 22:25:38 -0600

    arreglando footer

--- footer.php
+++ footer.php
@@ -50,6 +50,18 @@
 					</li>
 				</ul>
 			</div>
+			<div class="radio-buttons">
+				<h3>Tambien puedes escucharnos en:</h3>
+				<a href="http://centovacast.mexiserver.com:2199/tunein/electronic.pls"> <img alt="" src="https://electmind.files.wordpress.com/2015/03/winamp-logo-e1426830607462.jpg" /></a>
+				<a href="http://centovacast.mexiserver.com:2199/tunein/electronic.asx"> <img alt="" src="https://electmind.files.wordpress.com/2015/03/wm-e1426830598827.jpg" /></a>
+				<a href="	http://centovacast.mexiserver.com:2199/tunein/electronic.ram"> <img alt="" src="https://electmind.files.wordpress.com/2015/03/real-e1426830589141.jpg" /></a>
+				<a href="	http://centovacast.mexiserver.com:2199/tunein/electronic.qtl"> <img alt="" src="https://electmind.files.wordpress.com/2015/03/quicktime-logo-e1426830569126.jpg" /></a>
+				<a href="rtsp://204.12.193.98:1935/mobiles/electronic.stream"> 	<img src="https://www.mexiserver.com/players/mobiles/android.png" alt="" /></a>
+				<a href="http://204.12.193.98:1935/mobiles/electronic.stream/playlist.m3u8"> 	<img src="https://www.mexiserver.com/players/mobiles/iphone.png" alt="" /></a>
+				<a href="rtsp://204.12.193.98:1935/mobiles/electronic.stream"> 	<img src="https://www.mexiserver.com/players/mobiles/blackberry.png" alt="" /></a>
+				<a href="rtsp://204.12.193.98:1935/mobiles/electronic.stream"> 	<img src="https://www.mexiserver.com/players/mobiles/windows.png" alt="" /></a>
+				<a href="http://204.12.193.98:1935/mobiles/electronic.stream/playlist.m3u8"> 	<img src="https://www.mexiserver.com/players/mobiles/nokia.png" alt="" /></a>
+			</div>
 		</div>
 		<div class="site-info">
 			<p class="copyright">&copy; Todos los derechos reservados ElectronicMind <?php echo date("Y")?></p>
